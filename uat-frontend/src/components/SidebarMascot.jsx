e1cc006d1820a3af228eae8deffa2d5a592653cc
SalesWorks-Chris <chrisng730@gmail.com>
2026-08-18 09:24:10 +0800

    make sure mascot is not clickable

--- uat-frontend/src/components/SidebarMascot.jsx
+++ uat-frontend/src/components/SidebarMascot.jsx
@@ -1,44 +1,24 @@
-import { useState } from "react";
 import mascotImage from "../../public/peekqa-mascot-peeko.gif";
 import "../styles/SidebarMascot.css";
 
 export default function SidebarMascot({ size = 60, edgeAligned = false }) {
-  const [isBouncing, setIsBouncing] = useState(false);
-
-  const handleClick = () => {
-    setIsBouncing(false);
-
-    requestAnimationFrame(() => {
-      setIsBouncing(true);
-
-      setTimeout(() => {
-        setIsBouncing(false);
-      }, 450);
-    });
-  };
-
   return (
-    <button
-      type="button"
+    <div
       className={[
         "sidebar-mascot",
-        edgeAligned && "sidebar-mascot--edge-aligned",
-        isBouncing && "is-bouncing"
+        edgeAligned && "sidebar-mascot--edge-aligned"
       ]
         .filter(Boolean)
         .join(" ")}
-      onClick={handleClick}
-      aria-label="PeekQA mascot"
-      title="PeekQA mascot"
+      aria-hidden="true"
       style={{ "--mascot-size": `${size}px` }}
     >
-
       <img
         src={mascotImage}
         className="sidebar-mascot__image"
         alt=""
         draggable={false}
       />
-    </button>
+    </div>
   );
 }
